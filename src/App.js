5f054f67b14fcb2930cd7d41140bb8df74ad5781
ilyabanan <ibatuk53@gmail.com>
2022-10-11 16:30:29 +0300

    update 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-
+import './styles/index.scss';
 
 function App() {
   return (
     <div className="wrapper">
-        <header className="header">
+      <header className="header">
+        <div class="header__container _container">
+          <div className="header__block">
+            <div className="header__logo">
+              <a href="#">LOGO</a>
+            </div>
+            <div className="header__input">
+              <input type="text" />
+            </div>
+            <ul className="header__menu">
+              <li><a href="#" className='header__link'>a</a></li>
+              <li><a href="#" className='header__link'>b</a></li>
+              <li><a href="#" className='header__link'>c</a></li>
+            </ul>
+            <div className="header__icon">
+              <a href="#"><img src="./img/icon/user.svg" alt="user" /></a>
+              <a href="#"><img src="./img/icon/cart.svg" alt="cart" /></a>
 
-        </header>
-        <main className="main">
+            </div>
+            <button className="header__btn _btn">Login</button>
+          </div>
+        </div>
+      </header>
 
-        </main>
-        <footer className="footer">
-          
-        </footer>
+      <main className="main">
+
+
+      </main>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
